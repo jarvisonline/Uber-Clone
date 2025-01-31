30293e1e3c51969d307d8fbda3f8b8e55ad350c7
jarvisonline <98217852+jarvisonline@users.noreply.github.com>
2025-01-31 17:48:50 +0530

    backend-3

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,8 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/user.routes");
 const captainRoutes = require("./routes/captain.routes");
+const mapsRoutes = require("./routes/maps.routes");
+const rideRoutes = require("./routes/ride.routes");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,5 +19,7 @@ app.get("/", (req, res) => {
 
 app.use("/users", userRoutes);
 app.use("/captains", captainRoutes);
+app.use("/maps", mapsRoutes);
+app.use("/rides", rideRoutes);
 
 module.exports = app;
---
+++ Backend/routes/ride.routes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const { body } = require("express-validator");
+const rideController = require("../controllers/ridesController");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+router.post(
+  "/create",
+  authMiddleware.authUser,
+  body("pickup")
+    .isString()
+    .isLength({ min: 3 })
+    .withMessage("Invalid pickup address"),
+  body("destination")
+    .isString()
+    .isLength({ min: 3 })
+    .withMessage("Invalid destination address"),
+  body("vehicleType")
+    .isString()
+    .isIn(["auto", "car", "moto"])
+    .withMessage("Invalid vehicle type"),
+  rideController.createRide
+);
+
+module.exports = router;
